663a3b97e5d7a9a14dc2f11628a07e336734bc65
Tim Ryan <tpryan_1965@yahoo.com>
2013-12-08 16:54:28 -0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,17 @@ SampleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   root  'static_pages#home'
-  resources :users
-  resources :sessions,    only: [:new, :create, :destroy]
-  resources :microposts,  only: [:create, :destroy]
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
